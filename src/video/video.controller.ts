d7dd0a3d63944f9187eb20501f6cba2ac889488c
denisbogoslovskiy <bogoslovskiydenis@gmail.com>
2023-01-10 11:36:30 +0200

    fix eslint , refactoring

--- src/video/video.controller.ts
+++ src/video/video.controller.ts
@@ -1,34 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { VideoService } from './video.service';
-import { CreateVideoDto } from './dto/create-video.dto';
-import { UpdateVideoDto } from './dto/update-video.dto';
+import { Controller } from '@nestjs/common'
 
 @Controller('video')
-export class VideoController {
-  constructor(private readonly videoService: VideoService) {}
-
-  @Post()
-  create(@Body() createVideoDto: CreateVideoDto) {
-    return this.videoService.create(createVideoDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.videoService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.videoService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateVideoDto: UpdateVideoDto) {
-    return this.videoService.update(+id, updateVideoDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.videoService.remove(+id);
-  }
-}
+export class VideoController {}

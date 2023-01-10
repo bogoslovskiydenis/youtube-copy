b287d0753bbdea883a9226821bdb908a77b4f472
denisbogoslovskiy <bogoslovskiydenis@gmail.com>
2023-01-10 10:54:34 +0200

    backend models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
+import { VideoModule } from './video/video.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, VideoModule, CommentModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })

daef53686cd1d164c58a09dcc446973cd8ed3119
denisbogoslovskiy <bogoslovskiydenis@gmail.com>
2023-02-13 13:39:28 +0200

    video model

--- src/video/video.model/video.model.ts
+++ src/video/video.model/video.model.ts
@@ -1,1 +1,30 @@
-export class VideoModel {}
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses'
+import { prop } from '@typegoose/typegoose'
+
+export interface VideoModel extends Base {}
+
+export class VideoModel extends TimeStamps {
+	@prop({ unique: true })
+	name: string
+
+	@prop()
+	isPublic: string
+
+	@prop({ default: 0 })
+	view?: number
+
+	@prop({ default: 0 })
+	like?: number
+
+	@prop({ default: 0 })
+	dislike?: number
+
+	@prop()
+	description: number
+
+	@prop()
+	videoPath: string
+
+	@prop()
+	posterPath: string
+}

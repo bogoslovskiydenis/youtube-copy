f68d0778bf1240e0935022bcf804c074de0097e5
denisbogoslovskiy <bogoslovskiydenis@gmail.com>
2023-02-13 13:31:59 +0200

    user model

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { Module } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+import { TypegooseModule } from 'nestjs-typegoose'
+import { UserModel } from '../user/user.model/user.model'
+import { ConfigModule } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService]
+	controllers: [AuthController],
+	providers: [AuthService],
+	imports: [
+		TypegooseModule.forFeature([
+			{
+				typegooseClass: UserModel,
+				schemaOptions: {
+					collection: 'User',
+				},
+			},
+		]),
+		ConfigModule,
+		JwtModule,
+	],
 })
 export class AuthModule {}
--- src/user/user.model/user.model.ts
+++ src/user/user.model/user.model.ts
@@ -1,1 +1,33 @@
-export class UserModel {}
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses'
+import { prop } from '@typegoose/typegoose'
+
+export interface UserModel extends Base {}
+
+export class UserModel extends TimeStamps {
+	@prop({ unique: true })
+	email: string
+
+	@prop()
+	password: string
+
+	@prop({ unique: true })
+	name: string
+
+	@prop()
+	isVerified: string
+
+	@prop()
+	subcribersCount: number
+
+	@prop()
+	description: string
+
+	@prop()
+	location: string
+
+	@prop()
+	banner: string
+
+	@prop()
+	avatarPath: string
+}
